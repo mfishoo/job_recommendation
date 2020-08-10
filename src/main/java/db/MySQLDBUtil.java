f3e52c71a030e3990913dfe0e0ba4d78d05b87be
mfishoo <65449903+mfishoo@users.noreply.github.com>
2020-08-11 01:03:21 +0800

    Update MySQLDBUtil.java

--- src/main/java/db/MySQLDBUtil.java
+++ src/main/java/db/MySQLDBUtil.java
@@ -1,22 +1,14 @@
 package db;
 
 public class MySQLDBUtil {
-	// db2
-	private static final String INSTANCE = "job-recommendation.cxmqbaeboy9z.us-east-2.rds.amazonaws.com";
+	
+	private static final String INSTANCE = "";
 	private static final String PORT_NUM = "3306";
 	public static final String DB_NAME = "job_recommendation";
-	private static final String USERNAME = "mfishoo";
-	private static final String PASSWORD = "job-recommendation";
+	private static final String USERNAME = "";
+	private static final String PASSWORD = "";
 	public static final String URL = "jdbc:mysql://" + INSTANCE + ":" + PORT_NUM + "/" + DB_NAME + "?user=" + USERNAME
 			+ "&password=" + PASSWORD + "&autoReconnect=true&serverTimezone=UTC";
 
-	// db1
-//	private static final String INSTANCE = "laiproject-instance.cxmqbaeboy9z.us-east-2.rds.amazonaws.com";
-//	private static final String PORT_NUM = "3306";
-//	public static final String DB_NAME = "laiproject";
-//	private static final String USERNAME = "admin";
-//	private static final String PASSWORD = "laiprojectadmin";
-//	public static final String URL = "jdbc:mysql://" + INSTANCE + ":" + PORT_NUM + "/" + DB_NAME + "?user=" + USERNAME
-//			+ "&password=" + PASSWORD + "&autoReconnect=true&serverTimezone=UTC";
 
 }
